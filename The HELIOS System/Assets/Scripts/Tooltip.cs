ea30fc84b1850ad4164569e1527b4f2e4caf6326
Joseph Washum <Jkwashu@gmail.com>
2025-04-23 23:26:41 -0500

    Created new scripts for adding Tooltip functionality to organisms

---
+++ The HELIOS System/Assets/Scripts/Tooltip.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tooltip : MonoBehaviour
+{
+    private HungerScript hungerScript;
+    private MovementSteer movementScript;
+    
+    private void Start()
+    {
+        // Get references to the necessary components
+        hungerScript = GetComponent<HungerScript>();
+        movementScript = GetComponent<MovementSteer>();
+    }
+    
+    private void OnMouseEnter()
+    {
+        if (hungerScript != null && movementScript != null)
+        {
+            string tooltipText = GenerateTooltipText();
+            TooltipSystem.Instance.Show(tooltipText);
+        }
+        else if (GetComponent<PlantScript>() != null)
+        {
+            PlantScript plantScript = GetComponent<PlantScript>();
+            string tooltipText = GeneratePlantTooltipText(plantScript);
+            TooltipSystem.Instance.Show(tooltipText);
+        }
+    }
+    
+    private void OnMouseExit()
+    {
+        TooltipSystem.Instance.Hide();
+    }
+    
+    private string GenerateTooltipText()
+    {
+        string behaviorText = "Behavior: ";
+        
+        if (movementScript.isHunting)
+        {
+            behaviorText += "Hunting";
+        }
+        else if (movementScript.isWandering)
+        {
+            behaviorText += "Wandering";
+        }
+        else if (movementScript.isAvoidingPredator)
+        {
+            behaviorText += "Avoiding predator";
+        }
+        
+        return $"<b>{gameObject.name.Split('(')[0]}</b>\n" +
+               $"Speed: {movementScript.speed}\n" +
+               $"Hunger: {hungerScript.hunger:F1}/{hungerScript.feedThreshold:F1}\n" +
+               behaviorText;
+    }
+    
+    private string GeneratePlantTooltipText(PlantScript plantScript)
+    {
+        string growthStatus = plantScript.isFullyGrown ? "Fully grown" : 
+                             $"Growing ({plantScript.growthLevel:F1}/{plantScript.maxGrowthLevel:F1})";
+        
+        return $"<b>{gameObject.name.Split('(')[0]}</b>\n" +
+               $"Type: Plant\n" +
+               $"Status: {growthStatus}";
+    }
+}
---
+++ The HELIOS System/Assets/Scripts/TooltipSystem.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TooltipSystem : MonoBehaviour
+{
+    public static TooltipSystem Instance { get; private set; }
+    
+    [SerializeField] private GameObject tooltipGameObject;
+    [SerializeField] private TextMeshProUGUI tooltipText;
+    [SerializeField] private RectTransform backgroundRectTransform;
+    [SerializeField] private float padding = 10f;
+    [SerializeField] private Canvas canvas;
+    
+    private void Awake()
+    {
+        // Singleton pattern
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        Instance = this;
+        
+        // Initially hide the tooltip
+        tooltipGameObject.SetActive(false);
+    }
+    
+    private void Update()
+    {
+        // Follow mouse position
+        if (tooltipGameObject.activeSelf)
+        {
+            Vector2 mousePosition = Input.mousePosition;
+            
+            // Convert from screen to canvas space
+            if (canvas.renderMode == RenderMode.ScreenSpaceOverlay)
+            {
+                transform.position = mousePosition;
+            }
+            else if (canvas.renderMode == RenderMode.ScreenSpaceCamera)
+            {
+                Vector2 localPoint;
+                RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                    canvas.transform as RectTransform, mousePosition, canvas.worldCamera, out localPoint);
+                transform.position = canvas.transform.TransformPoint(localPoint);
+            }
+            
+            // Adjust position to prevent tooltip from going off-screen
+            LayoutRebuilder.ForceRebuildLayoutImmediate(backgroundRectTransform);
+            Vector2 pivotOffset = new Vector2(backgroundRectTransform.pivot.x * backgroundRectTransform.rect.width, 
+                                             backgroundRectTransform.pivot.y * backgroundRectTransform.rect.height);
+            
+            Vector2 tooltipSize = backgroundRectTransform.rect.size;
+            
+            Vector2 screenPosition = mousePosition + new Vector2(20, 20) - pivotOffset;
+            
+            if (screenPosition.x + tooltipSize.x > Screen.width)
+            {
+                screenPosition.x = mousePosition.x - tooltipSize.x - 20 - pivotOffset.x;
+            }
+            
+            if (screenPosition.y + tooltipSize.y > Screen.height)
+            {
+                screenPosition.y = mousePosition.y - tooltipSize.y - 20 - pivotOffset.y;
+            }
+            
+            transform.position = screenPosition;
+        }
+    }
+    
+    public void Show(string content)
+    {
+        tooltipGameObject.SetActive(true);
+        tooltipText.text = content;
+        
+        LayoutRebuilder.ForceRebuildLayoutImmediate(backgroundRectTransform);
+    }
+    
+    public void Hide()
+    {
+        tooltipGameObject.SetActive(false);
+    }
+}
